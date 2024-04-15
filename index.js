804246918de08da10005e60a80c3da6f9c6221d9
Pankaj Prajapati <114794372+pankaj143p@users.noreply.github.com>
2024-04-15 12:22:48 +0530

    add first server site

---
+++ index.js
@@ -0,0 +1,23 @@
+require('dotenv').config()
+const express = require('express');
+const app = express();
+const port = 4000;
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+    }
+);
+app.get('/instagram', (req, res) => {
+    res.send("welcome to instagram");
+});
+app.get('/*', (req, res) => {
+    res.send("welcome to wrong page");
+});
+
+app.listen(process.env.PORT, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+});
+
+
+
+
